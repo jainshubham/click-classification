4356ae0e41d24f08aece2dd357b6c9ed819b8e4e
Shubham <shubham2006@gmail.com>
2017-10-06 00:17:37 +0530

    close to the best I can do

--- click_classification.py
+++ click_classification.py
@@ -1,7 +1,8 @@
 import ast
 import pandas
 from sklearn import svm
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, f1_score
+from collections import defaultdict, Counter
 
 file_path = '/home/shubhamjain/Desktop/db_click_classification/ad_data.csv'
 
@@ -25,7 +26,7 @@ class click_classification(object):
         self.data_frame = pandas.DataFrame(data)
 
 
-    def data_preparation(self):
+    def transform_data(self):
         """
         """
         # fix spelling mistake in advertiser_name.
@@ -33,7 +34,7 @@ class click_classification(object):
         
         # get correlation between similar looking columns
         dd = pandas.get_dummies(self.data_frame[['advertiser_name', 'cat_id', 'category', 'channel_id', 'communication_line']], drop_first=True)
-        print(dd.corr())
+        # print(dd.corr())
         
         # drop advertiser_name and communication_line as perfectly corelated with category.
         self.data_frame = self.data_frame.drop('advertiser_name', 1)
@@ -47,18 +48,68 @@ class click_classification(object):
         
         # convert time to 6 quaters
         self.data_frame.insert_time = pandas.Categorical(self.data_frame.insert_time.apply(lambda x: int(int(x[11:13])/4)))
-        
-        # sesssion_id story_id
+       
+        # transform session id to feature
+        ctr = Counter(self.data_frame.sesssion_id) 
+        click_dict = defaultdict(int)
+        impression_dict = defaultdict(int)
+        for row in self.data_frame[['sesssion_id', 'event_type']].itertuples():
+            if row.event_type == 'click':
+                click_dict[row.sesssion_id] += 1
+            if row.event_type == 'impression':
+                impression_dict[row.sesssion_id] += 1
+        self.data_frame.sesssion_id_click_count = self.data_frame.sesssion_id.apply(lambda x: click_dict[x])
+        self.data_frame.sesssion_id_impression_count = self.data_frame.sesssion_id.apply(lambda x: ctr[x]-click_dict[x])
+
+
+        # transform session id to feature
+        ctr = Counter(self.data_frame.sesssion_id) 
+        click_dict = defaultdict(int)
+        impression_dict = defaultdict(int)
+        for row in self.data_frame[['sesssion_id', 'event_type']].itertuples():
+            if row.event_type == 'click':
+                click_dict[row.sesssion_id] += 1
+            if row.event_type == 'impression':
+                impression_dict[row.sesssion_id] += 1
+        self.data_frame.loc[:,'sesssion_id_click_count'] = self.data_frame.sesssion_id.apply(lambda x: click_dict[x])
+        self.data_frame.loc[:,'sesssion_id_impression_count'] = self.data_frame.sesssion_id.apply(lambda x: ctr[x]-click_dict[x])
+
+        # transform story_id to feature
+        ctr = Counter(self.data_frame.story_id) 
+        click_dict = defaultdict(int)
+        impression_dict = defaultdict(int)
+        for row in self.data_frame[['story_id', 'event_type']].itertuples():
+            if row.event_type == 'click':
+                click_dict[row.story_id] += 1
+            if row.event_type == 'impression':
+                impression_dict[row.story_id] += 1
+        self.data_frame.loc[:,'story_id_click_count'] = self.data_frame.story_id.apply(lambda x: click_dict[x])
+        self.data_frame.loc[:,'story_id_impression_count'] = self.data_frame.story_id.apply(lambda x: ctr[x]-click_dict[x])
+
+        # sesssion_id story_id are still not included in out analysis.
         dd = pandas.get_dummies(self.data_frame[['event_type', 'ad_id', 'category', 'channel_id', 'insert_time', 'title']], drop_first=True)
-        print(dd.corr())        
+        print(dd.corr()[['event_type_impression']])
+        # quite low correlation coefficients for everything.      
 
         
     def classify(self):
         """
         """
         X=pandas.get_dummies(self.data_frame[['ad_id', 'category', 'channel_id', 'insert_time', 'title']], drop_first=True)
+        X.loc[:,'sesssion_id_click_count'] = self.data_frame.sesssion_id_click_count
+        X.loc[:,'sesssion_id_impression_count'] = self.data_frame.sesssion_id_impression_count
+        #X.loc[:,'story_id_click_count'] = self.data_frame.story_id_click_count
+        #X.loc[:,'story_id_impression_count'] = self.data_frame.story_id_impression_count
+        print(X.columns)
         y=pandas.get_dummies(self.data_frame[['event_type']], drop_first=True)
-
-        mysvm = svm.SVC(kernel='rbf').fit(X,y)
+        mysvm = svm.SVC(kernel='linear', class_weight={1: 1, 0: 100}, C=1).fit(X,y)
         mysvm_pred = mysvm.predict(X)
         print(confusion_matrix(mysvm_pred, y))
+        print(f1_score(y, mysvm_pred))
+
+
+def run():
+    cc = click_classification()
+    cc.get_data()
+    cc.transform_data()
+    cc.classify()
--- readme.md
+++ readme.md
@@ -1,11 +1,48 @@
 
 
-pd.describe()
-
+A. pd.describe()
                ad_id advertiser_name  cat_id  category channel_id communication_line  event_type           insert_time plateform sesssion_id   story_id                                              title
 count         100000          100000  100000    100000     100000             100000      100000                100000    100000      100000     100000                                             100000
 unique             8               3       1         2          2                  3           2                 20134         1       81531       4909                                                  8
 top     138209154678        Adda.com          Adda.com        521           Adda.com  impression  2017-08-22T06:16:06Z       wap              120151885  10 मिनट में 6 डिजिट कैश प्राइज़ जीतने के लिए ख...
 freq           18996           85645  100000     85645      86092              85645       99370                    38    100000          60      14651                                              18996
 
+advertiser_name is corrected to contain only firstwall and none FirstWall.
 
+B. Co-relation 
+                              advertiser_name_firstwall  category_firstwall  channel_id_521  communication_line_Adda.com  communication_line_Firstwall
+advertiser_name_firstwall                      1.000000            1.000000       -0.981749                    -1.000000                      0.163673
+category_firstwall                             1.000000            1.000000       -0.981749                    -1.000000                      0.163673
+channel_id_521                                -0.981749           -0.981749        1.000000                     0.981749                      0.026933
+communication_line_Adda.com                   -1.000000           -1.000000        0.981749                     1.000000                     -0.163673
+communication_line_Firstwall                   0.163673            0.163673        0.026933                    -0.163673                      1.000000
+
+
+1. Communication_line_Adda.com is perfectly corelated to negative of advertiser_name_firstwall. This implies that all the missing values in communication_line are firstwall.
+2. communication_line, category and communication_line line are corelated therefore dropping advertiser_name and communication_line.
+3. Cat_id is empty hence dropping.
+4. Plateform is a constant hence dropping.
+
+                                                    event_type_impression
+event_type_impression                                            1.000000
+ad_id_138209024889                                              -0.006032
+ad_id_138209116249                                               0.007216
+ad_id_138209154384                                               0.008372
+ad_id_138209154678                                               0.014394
+ad_id_138209169641                                               0.008436
+ad_id_138209171096                                               0.012242
+ad_id_138209171162                                               0.009843
+category_firstwall                                              -0.060399
+channel_id_521                                                   0.060039
+insert_time_1                                                    0.040965
+insert_time_2                                                   -0.027901
+insert_time_3                                                   -0.027526
+insert_time_4                                                   -0.006620
+insert_time_5                                                   -0.010066
+title_22 साल की ये लड़की जैसे ही जेल में गई कैद...              -0.060039
+title_?????? ?? ???? ?? ??????, ?????? ???? Add...               0.000252
+title_अमीर बनिए- मेहनत कीजिए या स्मार्ट खेलिए                    0.009843
+title_अमीरों के क्लब से जुड़िए, ज्वाइन करें Add...               0.008370
+title_पोकर खेलिए और जीतिए अपनी सैलरी से कहीं ज्...               0.007216
+title_पोकर खेलें और जीती राशि को अपने बैंकखाते ...               0.007105
+title_मैंने चुना 10 मिनट में 10,000 रुपए बनाना                   0.012242
